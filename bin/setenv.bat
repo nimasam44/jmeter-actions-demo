1be7928d01beac22a3b8bc0214b69a81fcc42ce7
NimaSam <nima.samaranayake.nima@gmail.com>
2025-03-02 15:39:23 +1030

    Add set environment.bat with JAVA_HOME and PATH

---
+++ bin/setenv.bat
@@ -0,0 +1,4 @@
+@REM set the \java-home and run jmeter
+set JAVA_HOME=C:\Program Files\Java\jdk-11
+set PATH=%JAVA_HOME%\bin;%PATH%
+cd C:\Users\cws_dn_localadmin\Documents\JMetereTraining\Day1\apache-jmeter-5.6.3\bin
